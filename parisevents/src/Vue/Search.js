2250748a87c2cb57f68ce537d5dc9eb7a3392b4d
Andrea <Andrea Jovinac>
2021-09-10 12:13:08 +0200

    React_Click_Event_detail
    Ajouter la modal sur l'event recherche..

--- parisevents/src/Vue/Search.js
+++ parisevents/src/Vue/Search.js
@@ -5,26 +5,54 @@ import Eventsdetail from '../Components/Eventdetail';
 import Navbar from '../Components/Navbar';
 
 function Search() {
+  /* Tu créer des constantes 
+  Le Hook d’effet permet l’exécution d’effets de bord dans les fonctions composants :
+  La fonction setState permet de mettre à jour l’état local. Elle accepte une nouvelle valeur d’état local et planifie un nouveau rendu du composant.
+  
+  Souvent, les effets de bord créent des ressources qui nécessitent d’être nettoyées avant que le composant ne quitte l’écran, telles qu’un abonnement ou l’ID d’une horloge. 
+  Pour ce faire, la fonction fournie à useEffect peut renvoyer une fonction de nettoyage. Par exemple, pour créer un abonnement :
+  */
+
   const [datas, setDatas] = useState([]);
   const [donnees, setDonnees] = useState("");
   const Lienapi = "https://opendata.paris.fr/api/v2/catalog/datasets/que-faire-a-paris-/records?search=";
 
+  const [event_,setEvent_] = useState(null) /* Valeur par défaut et setEvent c'est la valeur que l'on change à chaque fois - UseState qui permet de mettre à jour la valeur à chaque fois*/
+  const [event_id,setEvent_id] = useState(null)
+  const [displayModal,setDisplayModal] = useState(false)
+
 /* Recuperer les données du input */
+
   const donneesinput = (e) => {
     let value = e.target.value;
     setDonnees(value);
   }
 
+  /* On teste  */
   console.log(donnees);
   
   useEffect(() => {
     fetch(Lienapi+donnees)
         .then(response => response.json())
-        .then(result => setDatas(result.records))
+        .then(result => {
+          setDatas(result.records)
+          const event_ = result.records[0].record.fields;
+          const event_id_ = result.records[0].record.id;
+          setEvent_(event_)
+          setEvent_id(event_id_)})
 }, [donnees]); // fin useEffect
 
+const openModal = () => { 
+  console.log(event_id);
+  setDisplayModal(true) 
+}
+const closeModal = () => { 
+  setDisplayModal(false) 
+}
+   /* On teste  */
+  console.log(datas);
 
-console.log(datas);
+
 
     return (
       <div className="App">
@@ -32,14 +60,14 @@ console.log(datas);
             <h1> Rechercher</h1>
             <label for="name">Tapez votre mot de recherche</label>
               <input type="text" id="name"  placeholder="Tapez votre mot" onInput={donneesinput}/>
-              <button>Rechercher</button>
+              <button onClick="">Rechercher</button>
           </section>
 
           <div className="search-result-container">
               { datas && datas.map((event) => (
-                  <Eventscomponents key={ event.record.id } id={ event.record.id } evenement={ event.record.fields } />
+                  <Eventscomponents key={ event.record.id }    id={ event.record.id }    evenement={ event.record.fields } onOpenModal={openModal}/>
               ))}
-
+          { displayModal && <Eventsdetail evenement={event_id, event_} onCloseModal={ closeModal }/> }
           </div>
       </div>
   );
